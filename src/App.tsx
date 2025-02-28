244f56eb761c1425755a3678270cb98aca9b4de1
Camputron <camposm.dev@gmail.com>
2025-02-28 13:05:47 -0500

    refactor: pages/index.ts
    - Abstracted all page components in Page constant (`pages/index.ts`)
    - Added PollJoin route (`/poll/join`)

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,29 @@
 import React from "react"
 import "./styles/App.css"
 import { Route, Routes } from "react-router-dom"
-import NotFound from "@/pages/NotFound"
 import NavBar from "./components/NavBar"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import GetStarted from "./pages/GetStarted"
-import PrivacyPolicy from "./pages/PrivacyPolicy"
-import TermsOfService from "./pages/TermsOfService"
-import Splash from "./pages/Splash"
-import PollLobby from "./pages/PollLobby"
-import Profile from "./pages/Profile"
-import PollView from "./pages/PollView"
-import PollResults from "./pages/PollResults"
-import PollEditor from "./pages/PollEditor"
-import Dashboard from "./pages/Dashboard"
-import Debug from "./pages/Debug"
+import { Page } from "./pages"
 
 export default function App() {
   return (
     <React.Fragment>
       <NavBar />
       <Routes>
-        <Route path='/' element={<Splash />} />
-        <Route path='/debug' element={<Debug />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/get-started' element={<GetStarted />} />
-        <Route path='/poll-lobby' element={<PollLobby />} />
-        <Route path='/privacy-policy' element={<PrivacyPolicy />} />
-        <Route path='/terms-of-service' element={<TermsOfService />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/poll/:id/edit' element={<PollEditor />} />
-        <Route path='/poll/:id/results' element={<PollResults />} />
-        <Route path='/poll/:id' element={<PollView />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path='/' element={<Page.Splash />} />
+        <Route path='/debug' element={<Page.Debug />} />
+        <Route path='/login' element={<Page.Login />} />
+        <Route path='/register' element={<Page.Register />} />
+        <Route path='/get-started' element={<Page.GetStarted />} />
+        <Route path='/poll-lobby' element={<Page.PollLobby />} />
+        <Route path='/privacy-policy' element={<Page.PrivacyPolicy />} />
+        <Route path='/terms-of-service' element={<Page.TermsOfService />} />
+        <Route path='/dashboard' element={<Page.Dashboard />} />
+        <Route path='/poll/join' element={<Page.PollJoin />} />
+        <Route path='/poll/:id/edit' element={<Page.PollEditor />} />
+        <Route path='/poll/:id/results' element={<Page.PollResults />} />
+        <Route path='/poll/:id' element={<Page.PollView />} />
+        <Route path='/profile' element={<Page.Profile />} />
+        <Route path='*' element={<Page.NotFound />} />
       </Routes>
     </React.Fragment>
   )
