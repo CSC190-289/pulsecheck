d6a320391e6123708cff92bb9367e26a1077f70f
Camputron <camposm.dev@gmail.com>
2025-03-03 12:01:10 -0500

    update(style): NavBar/Splash/About/Features/FAQs

--- src/components/splash/About.tsx
+++ src/components/splash/About.tsx
@@ -1,29 +1,38 @@
-import { Box, Typography } from "@mui/material"
+import { RA } from "@/styles"
+import { Box, Card, CardContent, Typography } from "@mui/material"
 import React from "react"
 
-function About(props: { ref: React.Ref<HTMLDivElement> }) {
+function About(props: { ref?: React.Ref<unknown> }) {
   return (
-    <div ref={props.ref}>
-      <Box textAlign={"left"}>
-        <Typography variant='h5' fontWeight={700}>
+    <Box textAlign={"center"} ref={props.ref}>
+      <RA.Bounce triggerOnce>
+        <Typography variant='h5' fontWeight={700} mb={2}>
           About
         </Typography>
-
-        <Typography variant='h6' sx={{ fontWeight: "bold" }}>
-          PulseCheck: Reinventing the Classroom Experience
-        </Typography>
-        <Typography variant='subtitle1' gutterBottom>
-          PulseCheck was created to address the challenges of engaging students
-          in large lecture halls. As classrooms grow, keeping students attentive
-          and involved becomes a monumental task. Our goal is to bridge the gap
-          between lecture and participation through interactive, real-time polls
-          and quizzes. Developed by a team of passionate educators and
-          developers, PulseCheck’s mission is to enhance student engagement,
-          provide instructors with instant feedback, and foster a more
-          interactive, personalized learning experience.
-        </Typography>
-      </Box>
-    </div>
+      </RA.Bounce>
+      <Card>
+        <CardContent>
+          <RA.Bounce triggerOnce>
+            <Typography fontWeight={700}>
+              Reinventing the Classroom Experience
+            </Typography>
+          </RA.Bounce>
+          <RA.Bounce triggerOnce>
+            <Typography variant='subtitle1' gutterBottom>
+              PulseCheck was created to address the challenges of engaging
+              students in large lecture halls. As classrooms grow, keeping
+              students attentive and involved becomes a monumental task. Our
+              goal is to bridge the gap between lecture and participation
+              through interactive, real-time polls and quizzes. Developed by a
+              team of passionate educators and developers, PulseCheck’s mission
+              is to enhance student engagement, provide instructors with instant
+              feedback, and foster a more interactive, personalized learning
+              experience.
+            </Typography>
+          </RA.Bounce>
+        </CardContent>
+      </Card>
+    </Box>
   )
 }
 
