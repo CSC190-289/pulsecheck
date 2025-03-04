ae4d54d0f9eac1ee32855710e8b33a9d0a6b7938
Bran7astic <budhanbrandon04@gmail.com>
2025-03-04 16:41:45 -0500

    Added create poll button

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,13 +1,25 @@
 import SignOutButton from "@/components/auth/SignOutButton"
-import { Container, Typography } from "@mui/material"
+import { useNavigate } from "react-router-dom"
+import {Button, Container, Typography } from "@mui/material"
 
 export default function Dashboard() {
+
+  const navigate = useNavigate();
+
+  const handleCreatePoll = () => {
+    
+    void navigate("/poll/:id/edit");
+  }
+
   return (
-    <Container>
+    <Container maxWidth="xs">
       <Typography>Dashboard Goes Here</Typography>
       <Typography variant='h4' gutterBottom>
         Different home page for logged in users
       </Typography>
+      <Button variant='contained'color='primary' fullWidth onClick={handleCreatePoll}> 
+            Create Poll
+      </Button>
       <SignOutButton />
     </Container>
   )
