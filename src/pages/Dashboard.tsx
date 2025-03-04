e46d45069edeff9906328fb0e57aca032631ed55
VerySirias <Verysirias@gmail.com>
2025-03-04 16:57:13 -0500

    feat: added user Dashboard, join poll, create poll

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,6 @@
 import SignOutButton from "@/components/auth/SignOutButton"
 import { useNavigate } from "react-router-dom"
-<<<<<<< HEAD
-import {Button, Container, Typography } from "@mui/material"
+import {Button, Container, Typography, Stack } from "@mui/material"
 
 export default function Dashboard() {
 
@@ -11,6 +10,9 @@ export default function Dashboard() {
     
     void navigate("/poll/:id/edit");
   }
+  const handleUserJoin = () => {
+    void navigate("/poll/join")
+  }
 
   return (
     <Container maxWidth="xs">
@@ -18,32 +20,29 @@ export default function Dashboard() {
       <Typography variant='h4' gutterBottom>
         Different home page for logged in users
       </Typography>
-      <Button variant='contained'color='primary' fullWidth onClick={handleCreatePoll}> 
-            Create Poll
-      </Button>
-=======
-import { Button, Container, Stack, TextField, Typography } from "@mui/material"
-
-
-
-export default function Dashboard() {
-  const navigate = useNavigate() 
-  const handleUserJoin = () => {
-    void navigate("/poll/join")
-  }
-  return (
-    <Container
-      maxWidth= 'xs'// need data for box
-    >
-        <Button
+      <Stack
+              component='form'
+              sx={{ m: 1 }} // margin for everything in the box
+              spacing={3}
+              noValidate
+              autoComplete='off'>
+      <Button
           variant='contained'
           color='primary'
           onClick={handleUserJoin}
-          fullWidth>
+          fullWidth
+          >
           Join Poll
         </Button>
->>>>>>> 3346708da2d1f3f0052df13c72180ce749ce85c4
+      <Button variant='contained'
+      color='primary' 
+      fullWidth 
+      onClick={handleCreatePoll}
+      > 
+            Create Poll
+      </Button>
       <SignOutButton />
+      </Stack>
     </Container>
   )
 }
--- src/pages/PollJoin.tsx
+++ src/pages/PollJoin.tsx
@@ -7,89 +7,88 @@ import { auth } from "@/core/api/firebase"
 import useSnackbar from "@/core/hooks/useSnackbar"
 
 export default function PollJoin() {
+  const navigate = useNavigate()
+  const [roomCode, setRoomCode] = useState<string>("")
+  const [displayName, setDisplayName] = useState<string>("")
+  const snackbar = useSnackbar()
 
-    const navigate = useNavigate()
-    const [roomCode, setRoomCode] = useState<string>("")
-    const [displayName, setDisplayName] = useState<string>("")
-    const snackbar = useSnackbar()
-  
-    const handleJoinClick = () => {
-      // void AddingUserToRoom(displayName, roomCode, () => {
-      //   void navigate("/poll-lobby")
-      // })
-      const aux = async () => {
-        try {
-          if (!roomCode.trim()) {
-            throw new Error("Room Code cannot be blank!")
-          }
-          if (!displayName.trim()) {
-            throw new Error("Display Name cannot be blank!")
-          }
-          const lobby = await findLobbyWithCode(roomCode)
-          const cred = await signInAnonymously(auth)
-          console.debug("lobby", lobby)
-          console.debug("cred", cred)
-          await createUser(cred.user.uid, displayName)
-          await joinLobby(lobby.id, cred.user.uid)
-          await navigate(`/poll/${lobby.id}/lobby`)
-        } catch (err: unknown) {
-          if (err instanceof Error) {
-            snackbar.show({
-              message: err.message,
-              type: "error",
-            })
-          } else {
-            snackbar.show({
-              message: "An unexpected error occured.",
-              type: "error",
-            })
-          }
+  const handleJoinClick = () => {
+    // void AddingUserToRoom(displayName, roomCode, () => {
+    //   void navigate("/poll-lobby")
+    // })
+    const aux = async () => {
+      try {
+        if (!roomCode.trim()) {
+          throw new Error("Room Code cannot be blank!")
+        }
+        if (!displayName.trim()) {
+          throw new Error("Display Name cannot be blank!")
+        }
+        const lobby = await findLobbyWithCode(roomCode)
+        const cred = await signInAnonymously(auth)
+        console.debug("lobby", lobby)
+        console.debug("cred", cred)
+        await createUser(cred.user.uid, displayName)
+        await joinLobby(lobby.id, cred.user.uid)
+        await navigate(`/poll/${lobby.id}/lobby`)
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          snackbar.show({
+            message: err.message,
+            type: "error",
+          })
+        } else {
+          snackbar.show({
+            message: "An unexpected error occured.",
+            type: "error",
+          })
         }
       }
-      void aux()
     }
-    return (
-      <Container
-        maxWidth='xs' //This allow the container to fit a certain size
-      >
-        <Typography
-          variant='h5'
-          textAlign='center'
-          marginTop={5}
-          marginBottom={5}>
-          Join Poll
-        </Typography>
-  
-        <Stack
-          component='form'
-          sx={{ m: 1 }} // margin for everything in the box
-          spacing={3}
-          noValidate
-          autoComplete='off'>
-          {/*FullWidth allows the button to extend to the xs maxwidth (styles it to match other button that have longer text or shorter)*/}
-          {/* We need to add the Join Function*/}
-          <TextField
-            id='room-code'
-            label='Room Code'
-            variant='outlined'
-            fullWidth
-            onChange={(e) => setRoomCode(e.target.value)}
-          />
-          <TextField
-            id='guest-name'
-            label='Display Name'
-            variant='outlined'
-            fullWidth
-            onChange={(e) => setDisplayName(e.target.value)}
-          />
-          <Button
-            variant='contained'
-            color='primary'
-            onClick={handleJoinClick}
-            fullWidth>
-            POLL UP
-          </Button>
-        </Stack>
-      </Container>
-    )
+    void aux()
+  }
+  return (
+    <Container
+      maxWidth='xs' //This allow the container to fit a certain size
+    >
+      <Typography
+        variant='h5'
+        textAlign='center'
+        marginTop={5}
+        marginBottom={5}>
+        Join Poll
+      </Typography>
+
+      <Stack
+        component='form'
+        sx={{ m: 1 }} // margin for everything in the box
+        spacing={3}
+        noValidate
+        autoComplete='off'>
+        {/*FullWidth allows the button to extend to the xs maxwidth (styles it to match other button that have longer text or shorter)*/}
+        {/* We need to add the Join Function*/}
+        <TextField
+          id='room-code'
+          label='Room Code'
+          variant='outlined'
+          fullWidth
+          onChange={(e) => setRoomCode(e.target.value)}
+        />
+        <TextField
+          id='guest-name'
+          label='Display Name'
+          variant='outlined'
+          fullWidth
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
+        <Button
+          variant='contained'
+          color='primary'
+          onClick={handleJoinClick}
+          fullWidth>
+          POLL UP
+        </Button>
+      </Stack>
+    </Container>
+  )
 }
