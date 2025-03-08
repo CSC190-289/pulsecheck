ea97bdf48f54c77c1556ea69c32cb888ded7914b
Bran7astic <budhanbrandon04@gmail.com>
2025-03-07 22:34:38 -0500

    Added card components for recent polls on Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,9 @@
 import SignOutButton from "@/components/auth/SignOutButton"
 import { useNavigate } from "react-router-dom"
-import { Button, Container, Typography, Stack } from "@mui/material"
+import { Button, Container, Typography, Stack, Box } from "@mui/material"
 import { collection, addDoc } from "firebase/firestore"
 import { db } from "@/core/api/firebase"
+import RecentPollCard from "../components/dashboard/RecentPollCard"
 //import { useAuthState } from 'react-firebase-hooks/auth';
 
 export default function Dashboard() {
@@ -31,29 +32,39 @@ export default function Dashboard() {
 
   return (
     <Container maxWidth='xs'>
-      <Typography>Dashboard Goes Here</Typography>
-      <Typography variant='h4' gutterBottom>
-        Different home page for logged in users
-      </Typography>
-      <Stack
-        sx={{ m: 1 }} // margin for everything in the box
-        spacing={3}>
-        <Button
-          variant='contained'
-          color='primary'
-          onClick={handleUserJoin}
-          fullWidth>
-          Join Poll
-        </Button>
-        <Button
-          variant='contained'
-          color='primary'
-          fullWidth
-          onClick={handleCreatePoll}>
-          Create Poll
-        </Button>
-        <SignOutButton />
-      </Stack>
+      <Box mb={16}>
+        <Typography>Dashboard Goes Here</Typography>
+        <Typography variant='h4' gutterBottom>
+          Different home page for logged in users
+        </Typography>
+        <Stack
+          sx={{ m: 1 }} // margin for everything in the box
+          spacing={3}>
+          <Button
+            variant='contained'
+            color='primary'
+            onClick={handleUserJoin}
+            fullWidth>
+            Join Poll
+          </Button>
+          <Button
+            variant='contained'
+            color='primary'
+            fullWidth
+            onClick={handleCreatePoll}>
+            Create Poll
+          </Button>
+          <SignOutButton />
+        </Stack>
+        <Stack sx={{ m: 1 }} spacing={3} gutterBottom>
+          <Typography variant='h6' align='left'>
+            Recent Polls
+          </Typography>
+          <RecentPollCard pollTitle='Getting Stated' result='0/100' />
+          <RecentPollCard pollTitle='Poll 2' result='90/100' />
+          <RecentPollCard pollTitle="Baby's First Poll" result='100/100' />
+        </Stack>
+      </Box>
     </Container>
   )
 }
